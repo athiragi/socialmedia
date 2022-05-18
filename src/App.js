68d9e89feef7947555a71a0584af31743aa5a279
athiragi <athirasnair72476@gmail.com>
2022-05-18 08:12:43 +0530

    searchpost

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 import Addpost from './component/Addpost';
+import Searchpost from './component/Searchpost';
 
 function App() {
   return (
     <div >
       <Addpost/>
+      <Searchpost/>
      
     </div>
   );
